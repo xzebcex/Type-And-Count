a3af67ee3a787ed8b3dd1054fb4893b5c2ced777
Aman Pandit <amanpandit3027@gmail.com>
2023-01-30 09:47:50 +0530

    Update count.py

--- count.py
+++ count.py
@@ -1,1 +1,10 @@
+# Type And Count
 
+# Prompt the user to start typing
+line = input('Start typing: ')
+
+# Count the number of spaces in the input and add 1 for the last word
+total_words = line.count(' ') + 1
+
+# Print the total number of words
+print(total_words)
